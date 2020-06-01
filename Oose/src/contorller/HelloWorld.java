5dd14575297e126374b0ed483a7524b8c9360ed2
llsrrll96 <58140426+llsrrll96@users.noreply.github.com>
2020-06-01 16:13:08 +0900

    ver1

---
+++ Oose/src/contorller/HelloWorld.java
@@ -0,0 +1,30 @@
+package contorller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloWorld extends HttpServlet{
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("service");
+		doGet(req, resp); //요청시 서비스 실행, doGet 분기
+	}
+	@Override
+	public void init() throws ServletException {//첫 요청때만 
+		System.out.print("init method\n");
+	}
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.print("doGet\n");
+	}
+
+}
